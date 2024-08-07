b332dbaaef7410852c196ca29cfff6582a0cafe3
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-08-07 04:25:52 +0300

    Improve storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,11 +55,11 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
-        """delete obj from __objects if it’s inside"""
+        """delete obj from __objects if it's inside"""
         if obj is not None:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
@@ -68,3 +68,21 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """A method to retrieve one object"""
+        for obj in self.__objects.values():
+            if isinstance(obj, cls) and obj.id == id:
+                return obj
+        return None
+
+    def count(self, cls=None):
+        """A method to count the number of objects in storage"""
+        counter = 0
+        if cls:
+            for obj in self.__objects.values():
+                if isinstance(obj, cls):
+                    counter += 1
+        else:
+            counter += len(self.__objects)
+        return counter
